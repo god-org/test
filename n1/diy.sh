10b2f2d8c93ed51d8c8250baf23d1b98c0bb4254
zzzzzzqqq8 <71330272+zzzzzzqqq8@users.noreply.github.com>
2024-11-24 07:02:45 +0800

    Update diy.sh

--- n1/diy.sh
+++ n1/diy.sh
@@ -4,13 +4,13 @@
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
 # Git sparse clone
-function git_sparse_clone() {
+git_sparse_clone() {
     branch="$1" repourl="$2" && shift 2
-    git clone --single-branch --depth=1 -b $branch --filter=blob:none --sparse $repourl
-    repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
-    cd $repodir && git sparse-checkout set $@
-    mv -f $@ ../package
-    cd .. && rm -rf $repodir
+    git clone --depth=1 -b "$branch" --single-branch --filter=blob:none --sparse "$repourl"
+    repodir=$(echo "$repourl" | awk -F '/' '{print $(NF)}')
+    cd "$repodir" && git sparse-checkout set "$@"
+    mv -f "$@" ../package
+    cd .. && rm -rf "$repodir"
 }
 
 # Add packages
