c0528fe57e1e208da84471c322a593c0cf31a8cb
zzzzzzqqq8 <71330272+zzzzzzqqq8@users.noreply.github.com>
2024-11-24 07:45:25 +0800

    Update diy.sh

--- n1/diy.sh
+++ n1/diy.sh
@@ -6,13 +6,13 @@ sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generat
 # Git sparse clone
 git_sparse_clone() {
     branch="$1" repourl="$2" && shift 2
-    git clone --depth=1 -b "$branch" --single-branch --filter=blob:none --sparse "$repourl"
-    repodir=$(echo "$repourl" | awk -F '/' '{print $(NF)}')
-    cd "$repodir" && git sparse-checkout set "$@"
+    git clone --depth=1 -b "$branch" --single-branch --filter=blob:none --sparse "$repourl" tmp_package
+    cd tmp_package && git sparse-checkout set "$@"
     mv -f "$@" ../package
-    cd .. && rm -rf "$repodir"
+    cd .. && rm -rf tmp_package
 }
 
 # Add packages
 git clone --single-branch --depth=1 https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
-git_sparse_clone master https://github.com/immortalwrt/packages net/rsync utils/docker-compose utils/sshpass utils/zstd
+git_sparse_clone master https://github.com/immortalwrt/packages net/rsync utils/zstd
+rm -rf feeds/packages/net/rsync feeds/packages/utils/zstd
