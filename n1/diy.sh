1b6cf9d74cb9d91a2ff52f2bd41325579869cd51
zzzzzzqqq8 <71330272+zzzzzzqqq8@users.noreply.github.com>
2026-02-04 16:20:01 +0800

    Update diy.sh

--- n1/diy.sh
+++ n1/diy.sh
@@ -3,7 +3,7 @@
 # Git 稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
     branch="$1" repourl="$2" && shift 2
-    git clone --depth=1 -b "$branch" --single-branch --filter=blob:none --sparse "$repourl"
+    git clone -b "$branch" --depth=1 --single-branch --filter=blob:none --sparse "$repourl"
     repodir=$(echo "$repourl" | awk -F '/' '{print $(NF)}')
     cd "$repodir" && git sparse-checkout set "$@"
     mv -f "$@" ../package
@@ -11,11 +11,11 @@ function git_sparse_clone() {
 }
 
 # 添加插件包
-git clone --single-branch --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
-git clone --single-branch --depth=1 https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
+git clone --depth=1 --single-branch https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+git clone --depth=1 --single-branch https://github.com/ophub/luci-app-amlogic package/luci-app-amlogic
 
-# 修改默认IP
-sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+# 修改默认 IP
+# sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
 # 修改主机名
 # sed -i 's/ImmortalWrt/n1/g' package/base-files/files/bin/config_generate
