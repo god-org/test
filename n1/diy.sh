0693e27e89153f9b76e970fab8649099a8714994
zzzzzzqqq8 <71330272+zzzzzzqqq8@users.noreply.github.com>
2026-02-15 00:02:45 +0800

    2026-02-15 00:02:45

--- n1/diy.sh
+++ n1/diy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Git 稀疏克隆，只克隆指定目录到本地
-function git_sparse_clone() {
-    branch="$1" repourl="$2" && shift 2
-    git clone -b "$branch" --depth=1 --single-branch --filter=blob:none --sparse "$repourl"
-    repodir=$(echo "$repourl" | awk -F '/' '{print $(NF)}')
-    cd "$repodir" && git sparse-checkout set "$@"
-    mv -f "$@" ../package
-    cd .. && rm -rf "$repodir"
-}
+# function git_sparse_clone() {
+#     branch="$1" repourl="$2" && shift 2
+#     git clone -b "$branch" --depth=1 --single-branch --filter=blob:none --sparse "$repourl"
+#     repodir=$(echo "$repourl" | awk -F '/' '{print $(NF)}')
+#     cd "$repodir" && git sparse-checkout set "$@"
+#     mv -f "$@" ../package
+#     cd .. && rm -rf "$repodir"
+# }
 
 # 添加插件包
 git clone --depth=1 --single-branch "https://github.com/kongfl888/luci-app-adguardhome" "package/luci-app-adguardhome"
